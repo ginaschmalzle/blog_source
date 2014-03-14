4091f1050fc4e640be8b3423915976c664cf8ba2
Gina Schmalzle <ginaschmalzle@gmail.com>
2014-03-13 21:56:25 -0400

    First Push

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,8 @@ AUTHOR = u'Gina Schmalzle'
 SITENAME = u'Gina Schmalzle'
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+REVERSE_CATEGORY_ORDER = True
+TIMEZONE = 'US/New York'
 
 DEFAULT_LANG = u'en'
 
@@ -19,13 +20,28 @@ TRANSLATION_FEED_ATOM = None
 LINKS =  (('Pelican', 'http://getpelican.com/'),
           ('Python.org', 'http://python.org/'),
           ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+          ('BOS Technologies', 'http://bostechnologies.com'),
+          ('Hacker School', 'http://hackerschool.com'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/GinaSchmalzle'),
+          ('Facebook', 'https://www.facebook.com/gschmalzle'),
+          ('LinkedIn', 'http://www.linkedin.com/pub/gina-schmalzle/13/8a/234'),)
 
 DEFAULT_PAGINATION = 10
 
+THEME = '/Users/ginaschmalzle/pelican-themes/blueidea'
+
+STATIC_PATHS = (['images','vectorprojector'])
+
+READERS = {'html': None}
+
+#PAGE_ORDER_BY = 'page-order'
+
+MENUITEMS = [('Home', '/category/home.html'),('Bio', '/category/bio.html'),('Pubs', '/category/pubs.html')]
+
+DISPLAY_PAGES_ON_MENU = False
+
+DISPLAY_CATEGORIES_ON_MENU = False
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
