4d4dc3930eeb5037c0adf11399da43d4af8b4566
Gina Schmalzle <ginaschmalzle@gmail.com>
2014-07-15 11:44:57 -0700

    Change Disqus

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = u'en'
 
-DISQUS_SITENAME = u'geodesygina.disqus.com'
+DISQUS_SITENAME = u'geodesygina'
 # Feed generation is usually not desired when developing
 FEED_DOMAIN = SITEURL
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
--- publishconf.py
+++ publishconf.py
@@ -21,7 +21,7 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 PLUGINS = [u"disqus_static"]
 
-DISQUS_SITENAME = u'geodesygina.disqus.com'
+DISQUS_SITENAME = u'geodesygina'
 DISQUS_SECRET_KEY = u'jaaoxoW3eIfofTJXNkqJumRS2WSFVXmy7ZmPIpnnc26GeuD49xEhXDv05FltujkC'
 DISQUS_PUBLIC_KEY = u'LWuUAL8MsMXypzANGbAiduBOKxey6KrZNFggyvQfhkjHCigVw2tA7hK3Y3xOXuVz'
 #GOOGLE_ANALYTICS = ""
