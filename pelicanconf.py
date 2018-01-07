c3b1f292dff7f316e33a784b7e0daaebc3545b40
Gina Schmalzle <ginaschmalzle@gmail.com>
2018-01-06 18:03:30 -0800

    More changes!

--- pelicanconf.py
+++ pelicanconf.py
@@ -39,7 +39,7 @@ SOCIAL = (('Twitter', 'https://twitter.com/GinaSchmalzle'),
 DEFAULT_PAGINATION = 10
 
 #THEME = '/Users/ginaschmalzle/pelican-themes/blueidea'
-THEME = '/home/fauns/pelican-themes/attila'
+THEME = '/home/fauns/attila'
 
 STATIC_PATHS = (['images','vectorprojector'])
 
